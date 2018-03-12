54273a719b89bc426fd31451636ab16d78e7ba66
Michael Pfeifer <mikepf97@gmail.com>
2018-03-11 20:53:00 -0600

    Added Fast Exponentiation with modding

---
+++ fastpow.cpp
@@ -0,0 +1,13 @@
+ll fastpow(ll base, ll exp, ll mod) {
+    ll res = 1;
+    while(exp > 0) {
+        if((exp & 1) == 1) {
+            res = (res * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+
+    return res % mod;
+}
+
