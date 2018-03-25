7d016a252a95e56b6c4961e07dc473c6506a1b63
Michael Pfeifer <mikepf97@gmail.com>
2018-03-25 14:16:08 -0600

    Added fast nCr and nPr

--- fastpow.cpp
+++ fastpow.cpp
@@ -11,3 +11,36 @@ ll fastpow(ll base, ll exp, ll mod) {
     return res % mod;
 }
 
+const ll MAXN = 1000000;
+ll factarr[MAXN+1];
+ll ifactarr[MAXN+1];
+
+void generate() {
+    factarr[0] = 1;
+    for(ll i = 1; i <= MAXN; i++) {
+        factarr[i] = (factarr[i-1] * i) % mod;
+    }
+    ifactarr[MAXN] = fastpow(factarr[MAXN], mod-2, mod);
+    for(int i = MAXN-1; i >= 0; i--) {
+        ifactarr[i] = (ifactarr[i+1] * (i+1)) % mod;
+    }
+}
+
+ll fact(ll n) {
+    return factarr[n];
+}
+
+ll ifact(ll n) {
+    return ifactarr[n];
+}
+
+ll ncr(ll n, ll r) {
+    if(n < 0 || r < 0 || r > n) return 0;
+    ll ans = (fact(n) * ifact(r)) % mod;
+    return (ans * ifact(n-r)) % mod;
+}
+
+ll npr(ll n, ll r) {
+    if(n < 0 || r < 0 || r > n) return 0;
+    return (fact(n) * ifact(n-r)) % mod;
+}
